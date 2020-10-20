560e7bb4c2fcf544cc6f6c9b8fa8fcc1cd054d02
USAMI Kenta <tadsan@zonu.me>
2020-10-20 13:53:49 +0900

    Impl rich Uopz class

--- src/Replacer/Uopz.php
+++ src/Replacer/Uopz.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Bag2\Doppel\Replacer;
 
 use Bag2\Doppel\Replacer;
+use Bag2\Doppel\Util\CallableHelper;
 use Closure;
 use function extension_loaded;
+use function uopz_add_function;
+use function uopz_del_function;
 use function uopz_set_return;
 use function uopz_unset_return;
 
@@ -21,6 +24,8 @@ use function uopz_unset_return;
  */
 class Uopz implements Replacer
 {
+    use CallableHelper;
+
     public function __construct()
     {
         // noop
@@ -31,9 +36,15 @@ class Uopz implements Replacer
      */
     public function register(?string $class_name, string $func_name, Closure $implementation): bool
     {
+        $stashed_name = $this->stashMethodName($class_name ?? '', $func_name);
+
         if ($class_name === null) {
+            uopz_add_function($stashed_name, Closure::fromCallable($func_name));;
+
             return uopz_set_return($func_name, $implementation, true);
         }
+
+        uopz_add_function($class_name, $stashed_name, Closure::fromCallable([$class_name, $func_name]));
 
         return uopz_set_return($class_name, $func_name, $implementation, true);
     }
@@ -43,9 +54,15 @@ class Uopz implements Replacer
      */
     public function restore(?string $class_name, string $func_name): bool
     {
+        $stashed_name = $this->stashMethodName($class_name ?? '', $func_name);
+
         if ($class_name === null) {
+            uopz_del_function($stashed_name);
+
             return uopz_unset_return($func_name);
         }
+
+        uopz_del_function($class_name, $stashed_name);
 
         return uopz_unset_return($class_name, $func_name);
     }
