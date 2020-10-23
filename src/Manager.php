d572ac069bc5aef5fac81c7f4ba9272f02fcb8f3
USAMI Kenta <tadsan@zonu.me>
2020-10-23 17:08:44 +0900

    Make Manager class implements ArrayAccess interface

--- src/Manager.php
+++ src/Manager.php
@@ -4,20 +4,24 @@ declare(strict_types=1);
 
 namespace Bag2\Doppel;
 
+use ArrayAccess;
 use Bag2\Doppel\Util\CallableHelper;
 use function debug_backtrace;
 use const DEBUG_BACKTRACE_IGNORE_ARGS;
 use Generator;
 use LogicException;
+use OutOfBoundsException;
 
 /**
  * Manager and Factory class of TestDouble
+ *
+ * @implements ArrayAccess<string,Doppel>
  *
  * @author USAMI Kenta <tadsan@zonu.me>
  * @copyright 2020 Baguette HQ
  * @license https://www.mozilla.org/en-US/MPL/2.0/ MPL-2.0
  */
-class Manager
+class Manager implements ArrayAccess
 {
     use CallableHelper;
 
@@ -88,5 +92,45 @@ class Manager
         }
 
         $this->finalized = true;
+    }
+
+    /**
+     * @param callable $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        [$class_name, $method_name] = static::extractClassMethod($offset);
+
+        return isset($this->doppels[$class_name][$method_name]);
+    }
+
+    /**
+     * @param callable $offset
+     * @return Doppel
+     */
+    public function offsetGet($offset)
+    {
+        [$class_name, $method_name] = static::extractClassMethod($offset);
+
+        return $this->doppels[$class_name][$method_name];
+    }
+
+    /**
+     * @param string $offset
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        throw new OutOfBoundsException('Do not add doppel by assign/offsetSet().');
+    }
+
+    /**
+     * @param string $offset
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        throw new OutOfBoundsException('Do not remove doppel by unset.');
     }
 }
